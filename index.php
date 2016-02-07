db3025da7ac8457069812ceb2c92d264e873dfae
Naveen Raman <dsfan414@gmail.com>
2016-02-07 11:34:15 -0500

    Basic Form

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
-<?php
-echo phpinfo();
-?>
+<html>
+	<head>
+		<link type="text/css" rel="stylesheet" href="stylesheet.css">
+                <title> Collaborate </title>
+
+        </head>
+
+        </body>
+                <p align="right">
+                        <?php
+                                if(isset($_SESSION["username"]))
+                                {
+                                        echo "Hello ".$_SESSION["username"];
+                                }
+                                else
+                                {
+                                        echo '<form action="login.php" name="loginForm">';
+                                        echo 'Username <input type="text" name="username">';
+                                        echo 'Password <input type="password" name="password">';
+                                        echo '</form>';
+                                }
+                        ?>
+                </p>
+                <h1> Login to see Anything </h1>
+        </body>
+
+</html>
